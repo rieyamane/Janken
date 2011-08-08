beb858ec033c86be94843cdbfdb6b3f31d5804e5
rieyamane <rieyamane@gmail.com>
2011-08-08 20:58:59 +0900

    じゃんけんゲーム created by xcode4

--- Janken/JankenViewController.h
+++ Janken/JankenViewController.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface JankenViewController : UIViewController
+@interface JankenViewController : UIViewController {
+    IBOutlet UIButton *gubtn;
+    IBOutlet UIButton *chbtn;
+    IBOutlet UIButton *pabtn;
+    IBOutlet UIButton *againbtn;
+    IBOutlet UILabel *meslbl;
+    IBOutlet UILabel *reslbl;
+    IBOutlet UILabel *aitelbl;
+    
+}
 
+-(IBAction)gubtn_down:(id)sender;
+-(IBAction)chbtn_down:(id)sender;
+-(IBAction)pabtn_down:(id)sender;
+-(IBAction)againbtn_down:(id)sender;
 @end
--- Janken/JankenViewController.m
+++ Janken/JankenViewController.m
@@ -10,6 +10,98 @@
 
 @implementation JankenViewController
 
+-(IBAction)gubtn_down:(id)sender {
+    meslbl.text = @"ぽいぽいぽいぽ ぽいぽいぽぴー！";
+    chbtn.hidden = YES;
+    pabtn.hidden = YES;
+    gubtn.hidden = YES;
+    aitelbl.hidden = NO;
+    reslbl.hidden = NO;
+    NSInteger res = arc4random() % 3;
+    if (res == 0) {
+        aitelbl.text = @"ぐー";
+        reslbl.text = @"あいこで・・・";
+        gubtn.hidden = NO;
+        chbtn.hidden = NO;
+        pabtn.hidden = NO;
+        againbtn.hidden = YES;
+    } else if (res == 1) {
+        reslbl.text = @"あなたのかち";
+        aitelbl.text = @"ちょき";
+        againbtn.hidden = NO;
+    } else {
+        reslbl.text = @"あなたのまけ";
+        aitelbl.text = @"ぱー";
+        againbtn.hidden = NO;
+    }
+}
+
+-(IBAction)chbtn_down:(id)sender {
+    meslbl.text = @"ぽいぽいぽいぽ ぽいぽいぽぴー！";
+    chbtn.hidden = YES;
+    pabtn.hidden = YES;
+    gubtn.hidden = YES;
+    aitelbl.hidden = NO;
+    reslbl.hidden = NO;
+    NSInteger res = arc4random() % 3;
+    if (res == 0) {
+        aitelbl.text = @"ぐー";
+        reslbl.text = @"あなたのまけ";
+        againbtn.hidden = NO;
+    } else if (res == 1) {
+        reslbl.text = @"あいこで・・・";
+        aitelbl.text = @"ちょき";
+        gubtn.hidden = NO;
+        chbtn.hidden = NO;
+        pabtn.hidden = NO;
+        againbtn.hidden = YES;
+    } else {
+        reslbl.text = @"あなたのかち";
+        aitelbl.text = @"ぱー";
+        againbtn.hidden = NO;
+    }
+
+}
+
+-(IBAction)pabtn_down:(id)sender {
+    meslbl.text = @"ぽいぽいぽいぽ ぽいぽいぽぴー！";
+    chbtn.hidden = YES;
+    pabtn.hidden = YES;
+    gubtn.hidden = YES;
+    aitelbl.hidden = NO;
+    reslbl.hidden = NO;
+    NSInteger res = arc4random() % 3;
+    if (res == 0) {
+        aitelbl.text = @"ぐー";
+        reslbl.text = @"あなたのかち";
+        againbtn.hidden = NO;
+    } else if (res == 1) {
+        reslbl.text = @"あなたのまけ";
+        aitelbl.text = @"ちょき";
+        againbtn.hidden = NO;
+    } else {
+        reslbl.text = @"あいこで・・・";
+        aitelbl.text = @"ぱー";
+        gubtn.hidden = NO;
+        chbtn.hidden = NO;
+        pabtn.hidden = NO;
+        againbtn.hidden = YES;
+    }
+    
+}
+
+-(IBAction)againbtn_down:(id)sender {
+    chbtn.hidden = NO;
+    gubtn.hidden = NO;
+    pabtn.hidden = NO;
+    meslbl.text = @"あやまんじゃんけん・・・";
+    aitelbl.text = @"";
+    reslbl.text = @"";
+    againbtn.hidden = YES;
+    
+}
+
+
 - (void)didReceiveMemoryWarning
 {
     // Releases the view if it doesn't have a superview.
@@ -20,13 +112,15 @@
 
 #pragma mark - View lifecycle
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    aitelbl.hidden = YES;
+    reslbl.hidden = YES;
+    againbtn.hidden = YES;
 }
-*/
 
 - (void)viewDidUnload
 {
